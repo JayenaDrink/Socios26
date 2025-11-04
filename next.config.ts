fed51443cd1bc88474e4965e8d375e4ef95d0c81
Nick Kligman <nickkligman@nicks-macbook-pro-2.home>
2025-11-04 14:39:20 +0100

    Exclude backup folders and Online from TypeScript build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Exclude backup directories and Online folder from build
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules',
+        '**/.git',
+        '**/backup_*',
+        '**/Online',
+        '**/backup_*/**',
+        '**/Online/**',
+      ],
+    };
+    return config;
+  },
+  // Exclude from TypeScript checking
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  // Exclude from ESLint checking
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
